7bb535bffd05809f12346614b1813fa00148c91f
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-03-16 19:22:07 -0400

    still need to test run code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,10 +2,29 @@ const express = require ('express')
 
 const router = express.Router();
 
-const burger = require('../models/burger.js')
+const orm = require("../config/orm")
 
-router.get('/', (req,res) =>{
+router.get('/', async (req,res) =>{
+    const burger = await orm.selectAll()
+     console.log(burger)
+
+     res.render('index', burger)
 
 })
 
-module.exports 
+router.post('/api/create', async (req,res) =>{
+    let input = req.body 
+    let result = await orm.insertOne()
+    console.log(result)
+    res.redirect('/')
+})
+
+router.post('.api/burger/:id', async (req, res)=>{
+    const id = req.params.id 
+
+    const updateBurger = await orm.updateOne(id)
+
+    res.redirect('/')
+})
+
+module.exports =router; 
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,20 @@
+// Have to import ORM to create functions that interact with Database
+
 const orm = require ('../config/orm.js');
 
-// call orm functions into burger.js 
+const burger = {
+
+    selectAll(cb){
+        orm.selectAll('burgers', (res) => cb(res));
+    }, 
+
+    insertOne(cb){
+        orm.insertOne(burger_name, (res)=> cb(res));
+    }, 
+
+    updateOne(cb){
+        orm.updateOne(burger_id, (res) => cb(res));
+    }
+
+}
+module.exports = burger;
